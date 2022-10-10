e8758501a23a3a572b9cff99a508e27c9510bab6
Harmohit Singh <2003mohit@gmail.com>
2022-10-10 19:05:10 +0530

    update info

--- CPP-1/CRSCalculator.cpp
+++ CPP-1/CRSCalculator.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main(){
     int apc, c1, l, b;
+    /*apc operand short for "Area Perimeter Calculator" for switch
+    c1 operand for if statement
+    l operand for length, side and radius
+    b operand for breadth*/
     cout<<"Welcome to Area and Perimeter Calculator!\nPress 1 for Rectangle\nPress 2 for Square\nPress 3 for Circle"<<endl;
     cin>>apc;
     switch (apc)
