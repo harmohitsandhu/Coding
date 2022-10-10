098acfd3bb245121e35938308711b46587176a52
Harmohit Singh <2003mohit@gmail.com>
2022-10-10 17:51:22 +0530

    update

---
+++ CPP-1/CRSCalculator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Welcome to Area and Perimeter Calculator!\nPress 1 for Rectangle\nPress 2 for Square\nPress 3 for Circle"<<endl;
+    
+    return 0;
+}
