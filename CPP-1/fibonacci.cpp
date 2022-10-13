cffa4fa0deb5f08e1c296d380f9d97b772686ccd
Harmohit Singh <2003mohit@gmail.com>
2022-10-14 00:41:38 +0530

    under progress

---
+++ CPP-1/fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+int main(){
+    int a,b;
+    cout<<"Welcome to Fibonacci Series Calculator!\nEnter the value till you want the find the fibonacci series = ";
+    cin>>a;
+    for (b=0; b<=a; b++)
+    {
+        cout<<b;
+    }
+    
+    return 0;
+}
