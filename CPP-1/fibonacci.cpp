e95cbbc5005f633f37d4584cfc10a2b21c837871
Harmohit Singh <2003mohit@gmail.com>
2022-10-14 16:05:24 +0530

    update

--- CPP-1/fibonacci.cpp
+++ CPP-1/fibonacci.cpp
@@ -3,12 +3,16 @@ using std::cout;
 using std::endl;
 using std::cin;
 int main(){
-    int a,b;
-    cout<<"Welcome to Fibonacci Series Calculator!\nEnter the value till you want the find the fibonacci series = ";
-    cin>>a;
-    for (b=0; b<=a; b++)
+    int i,n,n1=0,n2=1,n3;
+    cout<<"Welcome to Fibonacci Series Calculator!\nEnter the number of terms = ";
+    cin>>n;
+    cout<<"Fibonacci Series: \n"<<n1<<" "<<n2<<" ";
+    for (i=3; i<=n; i++)
     {
-        cout<<b;
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        cout<<n3<<" ";
     }
     return 0;
 }
