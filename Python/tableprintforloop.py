af0bab3f96828f99f3cec8c581e91ac687f13465
Harmohit Singh <2003mohit@gmail.com>
2022-11-02 17:49:02 +0530

    update

--- Python/tableprintforloop.py
+++ Python/tableprintforloop.py
@@ -1,7 +1,6 @@
 print("Welcome to Table maker!")
-a=int(input("Enter a number to display its mathematical table = "))
+a=float(input("Enter a number to display its mathematical table = "))
 b=int(input("Enter the limit to display its mathematical table = "))
-c=int()
-for b in range (c,b+1):
-    print(c,"x",a,"=",c*a,"\n")
-    c+=1
+c=float()
+for i in range (0,b+1):
+    print(a,"x",i,"=",i*a,"\n")
--- Python/tableprintwhileloop.py
+++ Python/tableprintwhileloop.py
@@ -1,7 +1,7 @@
 print("Welcome to Table maker!")
-a=int(input("Enter a number to display its mathematical table = "))
+a=float(input("Enter a number to display its mathematical table = "))
 b=int(input("Enter the limit to display its mathematical table = "))
 c=int()
 while(c<=b):
-    print(c,"x",a,"=",c*a,"\n")
+    print(a,"x",c,"=",c*a,"\n")
     c+=1
