e6ccdfff97ead2bd19780672730485866fd7422b
Harmohit Singh <2003mohit@gmail.com>
2022-11-08 00:32:09 +0530

    update

---
+++ CPP-1/fibonaccirecursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+int fib(int n){
+    int f;
+    if(n<=1)
+    {
+        return(n);
+    }
+    else
+    {
+        return fib(n-1)+fib(n-2);
+    }
+}
+int main(){
+    int n;
+    cout<<"Welcome to Fibonacci Calculator!\nEnter the number of terms = ";
+    cin>>n;
+    cout<<fib(n);
+    return 0;
+}
