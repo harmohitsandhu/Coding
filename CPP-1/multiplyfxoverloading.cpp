af19d7d5069969c8efade32ea623d464b084897a
Harmohit Singh <2003mohit@gmail.com>
2022-11-08 01:51:11 +0530

    update

--- CPP-1/multiplyfxoverloading.cpp
+++ CPP-1/multiplyfxoverloading.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 using std::cout;
 using std::cin;
+int fx(float,float);
 int main(){
-    int n1,n2;
+    float n1,n2,mult;
     cout<<"Welcome to function overloading experiment!\nPlease enter the value of first number = ";
     cin>>n1;
     cout<<"Enter the value of second number = ";
     cin>>n2;
+    mult=fx(n1,n2);
+    cout<<mult;
+    return 0;
+}
+int fx(float x,float y){
+    int c;
+    c=x*y;
+    cout<<"Value is = ";
+    return(c);
 }
